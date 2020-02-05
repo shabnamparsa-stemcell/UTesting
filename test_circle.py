09267289046d83197cc8620deff2b2b18f868e6b
shabnam.parsa <pourandokht99$>
2020-02-05 14:40:12 -0800

    a new file for testing

--- test_circle.py
+++ test_circle.py
@@ -9,8 +9,11 @@ class TestcircleArea(unittest.TestCase):
     def test_area(self):
         # test areas when radious is >=0
         self.assertAlmostEqual(circle_area(1), pi)
-        self.assertAlmostEqual(circle_area(0), 3) # on purpose so it fails
         self.assertAlmostEqual(circle_area(2.1), pi * 2.1 ** 2)
         self.assertAlmostEqual(circle_area(10), pi * 10 ** 2)
         self.assertAlmostEqual(circle_area(3), pi * 3 ** 2)
         self.assertAlmostEqual(circle_area(7), pi * 7 ** 2)
+
+    def test_area1(self):
+        # test areas when radious is >=0
+        self.assertAlmostEqual(circle_area(0), 3) # on purpose so it fails
