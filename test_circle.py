8de015817ec9fa3df44da08b708b2841c93d5a0f
shabnam.parsa <pourandokht99$>
2020-02-05 14:24:05 -0800

    a new file for testing

--- test_circle.py
+++ test_circle.py
@@ -12,3 +12,4 @@ class TestcircleArea(unittest.TestCase):
         self.assertAlmostEqual(circle_area(0), 3) # on purpose so it fails
         self.assertAlmostEqual(circle_area(2.1), pi * 2.1 ** 2)
         self.assertAlmostEqual(circle_area(10), pi * 10 ** 2)
+        self.assertAlmostEqual(circle_area(3), pi * 3 ** 2)
